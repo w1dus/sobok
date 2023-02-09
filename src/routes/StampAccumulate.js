6be2fc61ce75e19632b68695aa5d09635fa400f6
koreanleo <dhrn0623@naver.com>
2023-02-09 13:14:19 +0900

    stamAccumlate.js update

--- src/routes/StampAccumulate.js
+++ src/routes/StampAccumulate.js
@@ -1,13 +1,38 @@
 import Header from 'components/Header';
 import Navigation from 'components/Navigation';
-import React from 'react'; 
+import { dbService } from 'fbase';
+import { collection, getDocs, limit, query, where } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'; 
 import { useNavigate } from 'react-router-dom';
 import styles from"./StampAccumulate.module.css";
 
 
-function StampAccumulate(){
+function StampAccumulate({userObj}){
 
     let navigate = useNavigate();
+    
+    const [phoneNumber, setPhoneNumber] = useState("");    
+    
+    const getTelephone = async() => {
+        
+        const db_query = query( 
+            collection(dbService, "user"),
+            where("uid", "==" , userObj.uid),
+            limit(1)
+        )
+            
+        const telephone = await getDocs(db_query);
+        if(!telephone.empty){
+            telephone.forEach((document) => {
+                setPhoneNumber(document.data().telephone)
+            });
+        }
+    }
+    
+    useEffect(() => {
+        getTelephone();
+        
+    } , [])
 
     return (
         <>
@@ -15,8 +40,12 @@ function StampAccumulate(){
             <div className={styles.stampContainer}>
                 <p className={styles.title}>스템프 적립</p>
                 <img src={`${process.env.PUBLIC_URL}/images/sobok_logo_and_title.svg`} alt="소복 스탬프 적립 요청" className={styles.logo}/>
-                <p className={styles.phoneNumber}>010-0000-0000</p>
-                <p className={styles.help}>카페 직원분에게 해당 번호를 알려주세요.</p>
+                {(phoneNumber || phoneNumber === "") ? 
+                    <p className={styles.phoneNumber}>{phoneNumber.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)}</p>
+                    :
+                    <p className={styles.help}>[my]➡️[회원정보수정]에서 연락처를 등록해주세요.</p>
+                }
+                <p className={styles.help}> {userObj.email}   카페 직원분에게 해당 아이디를 보여주세요.</p>
 
                 <div className={styles.btnWrap}>
                     <button className={styles.okBtn} onClick={() => {navigate(-1)}}>확인</button>
